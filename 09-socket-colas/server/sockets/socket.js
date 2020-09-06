2beead00632a1686297a2d62dc2e2b6d80e9805a
KS <ksanchez@deipe.com>
2020-09-06 18:44:51 +0100

    Aplicacionde colas OK

--- 09-socket-colas/server/sockets/socket.js
+++ 09-socket-colas/server/sockets/socket.js
@@ -10,6 +10,19 @@ io.on('connection', (client) => {
         callback(siguienteTicket);
     });
 
-    client.emit('estadoActual', { actual: ticketControl.getUltimoTicket() });
+    client.emit('estadoActual', { actual: ticketControl.getUltimoTicket(), ultimos4: ticketControl.getUltimos4() });
+
+    client.on('atenderTicket', (data, callback) => {
+        const { escritorio } = data;
+        if (!escritorio) {
+            return callback({ err: true, mensaje: 'El escritorio es necesario' });
+        }
+        const atenderTicket = ticketControl.atenderTicket(escritorio);
+        callback(atenderTicket);
+
+        //Actualizar / notificar cambios en los ultimos 4 a todos los clientes conectados
+        client.broadcast.emit('ultimos4', { ultimos4: ticketControl.getUltimos4() });
+
+    });
 
 });
